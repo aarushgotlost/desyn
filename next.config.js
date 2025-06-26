3f66bbe771b7e96f0924e6534df9dd5c08a93617
Aarush Sahu <aarushsahupersonal@gmail.com>
2025-06-26 07:27:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -18,24 +18,6 @@ const nextConfig = {
       },
     ],
   },
-   // Required for ffmpeg.wasm to work
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 module.exports = nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -20,24 +20,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   // Required for ffmpeg.wasm to work
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ]
-  },
 };
 
 export default nextConfig;
